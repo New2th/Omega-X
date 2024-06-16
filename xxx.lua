b1a8f3db3bb471a3d3d1634ff7ff15ea3b53a404
New2th <73757279+New2th@users.noreply.github.com>
2024-06-15 19:44:52 -0700

    Create xxx.lua

---
+++ xxx.lua
@@ -0,0 +1,531 @@
+local Console =  loadstring(game:HttpGet("https://raw.githubusercontent.com/bloodball/-back-ups-for-libs/main/STX"))()
+local ConsoleLog = Console:Window({
+    Title = "[VY_CONSOLE] Xy-Hub 1.12",
+    Position = UDim2.new(0.5, 0, 0.5, 0),
+    DragSpeed = 12
+})
+
+ConsoleLog:Prompt({
+    Title = "Bypass injected UNC = 99%",
+    Type = "success"
+})
+wait(0.2)
+ConsoleLog:Prompt({
+    Title = "UDim2.new found",
+    Type = "success"
+})
+ConsoleLog:Prompt({
+    Title = "PlayerConnect()",
+    Type = "fail"
+})
+ConsoleLog:Prompt({
+    Title = "Toggle made sucessfully",
+    Type = "success"
+})
+ConsoleLog:Prompt({
+    Title = "brightness.new found",
+    Type = "success"
+})
+wait(0.2)
+ConsoleLog:Prompt({
+    Title = "wait(1) found",
+    Type = "success"
+})
+wait(0.2)
+ConsoleLog:Prompt({
+    Title = "getenv() not found",
+    Type = "fail"
+})
+wait(0.3)
+ConsoleLog:Prompt({
+    Title = "Bypass suceeded",
+    Type = "success"
+})
+wait(0.3)
+ConsoleLog:Prompt({
+    Title = "GetPlayerFromCharacter() found",
+    Type = "success"
+})
+wait(2)
+ConsoleLog:Prompt({
+    Title = "ver-b4a1093fd70b7c8d6e99fc83d9aea09640825e83390588a347bbd08d5d2b210c",
+    Type = "success"
+})
+wait(0.1)
+ConsoleLog:Prompt({
+    Title = "ver-aff4751ae6d06963a9850570d6671abd899f9db92083967973a2d128f12901b0",
+    Type = "warning"
+})
+wait(0.1)
+ConsoleLog:Prompt({
+    Title = "ver-f02ebcdef6e2beb7d15e34f4ebdb084f9b82e8198f830860a61613bfcb82536f",
+    Type = "fail"
+})
+wait(0.1)
+ConsoleLog:Prompt({
+    Title = "ver-1f6ead12bffa21b2ceccb450684decdd0ed21b5b7be3ed2fe834a8c0afa528e6",
+    Type = "fail"
+})
+wait(0.1)
+ConsoleLog:Prompt({
+    Title = "ver-c7e8995823b5e8919243a7a457490110763cb474e8541a8851d56afc9df5a200",
+    Type = "warning"
+})
+wait(0.1)
+ConsoleLog:Prompt({
+    Title = "ver-14beb321d64bc5e44e311711b17a034c6e89001538fe2581d1cb21da943439bd",
+    Type = "success"
+})
+wait(0.1)
+ConsoleLog:Prompt({
+    Title = "ver-e54652e99fec0b213ec70ef5c6cad1119a042831ead3c756f5846cfcee39e1d6",
+    Type = "success"
+})
+wait(0.1)
+ConsoleLog:Prompt({
+    Title = "ver-34974e290ce6e17c229dcfac26eb3c83227daece8e5d9694598f143b80464450",
+    Type = "success"
+})
+wait(0.1)
+ConsoleLog:Prompt({
+    Title = "ver-a483f69ad6f299e35fb9de84dda97c0822075d54eb5fe9e5814f728938f9f29c",
+    Type = "success"
+})
+wait(0.1)
+ConsoleLog:Prompt({
+    Title = "ver-81b31bcac3f542b257eef394ef59248469f589200c09398a196f1af00b478c42",
+    Type = "success"
+})
+wait(0.1)
+ConsoleLog:Prompt({
+    Title = "ver-b4a1093fd70b7c8d6e99fc83d9aea09640825e83390588a347bbd08d5d2b210c",
+    Type = "warning"
+})
+wait(0.1)
+ConsoleLog:Prompt({
+    Title = "ver-aff4751ae6d06963a9850570d6671abd899f9db92083967973a2d128f12901b0",
+    Type = "fail"
+})
+wait(0.1)
+ConsoleLog:Prompt({
+    Title = "ver-f02ebcdef6e2beb7d15e34f4ebdb084f9b82e8198f830860a61613bfcb82536f",
+    Type = "fail"
+})
+wait(0.1)
+ConsoleLog:Prompt({
+    Title = "ver-1f6ead12bffa21b2ceccb450684decdd0ed21b5b7be3ed2fe834a8c0afa528e6",
+    Type = "fail"
+})
+wait(0.1)
+ConsoleLog:Prompt({
+    Title = "ver-c7e8995823b5e8919243a7a457490110763cb474e8541a8851d56afc9df5a200",
+    Type = "fail"
+})
+wait(0.1)
+ConsoleLog:Prompt({
+    Title = "ver-14beb321d64bc5e44e311711b17a034c6e89001538fe2581d1cb21da943439bd",
+    Type = "fail"
+})
+wait(0.1)
+ConsoleLog:Prompt({
+    Title = "ver-e54652e99fec0b213ec70ef5c6cad1119a042831ead3c756f5846cfcee39e1d6",
+    Type = "warning"
+})
+wait(0.1)
+ConsoleLog:Prompt({
+    Title = "ver-34974e290ce6e17c229dcfac26eb3c83227daece8e5d9694598f143b80464450",
+    Type = "fail"
+})
+wait(0.1)
+ConsoleLog:Prompt({
+    Title = "ver-a483f69ad6f299e35fb9de84dda97c0822075d54eb5fe9e5814f728938f9f29c",
+    Type = "warning"
+})
+wait(0.1)
+ConsoleLog:Prompt({
+    Title = "ver-81b31bcac3f542b257eef394ef59248469f589200c09398a196f1af00b478c42",
+    Type = "fail"
+})
+ConsoleLog:Prompt({
+    Title = "ver-aff4751ae6d06963a9850570d6671abd899f9db92083967973a2d128f12901b0",
+    Type = "fail"
+})
+wait(0.1)
+ConsoleLog:Prompt({
+    Title = "ver-f02ebcdef6e2beb7d15e34f4ebdb084f9b82e8198f830860a61613bfcb82536f",
+    Type = "warning"
+})
+wait(0.1)
+ConsoleLog:Prompt({
+    Title = "ver-1f6ead12bffa21b2ceccb450684decdd0ed21b5b7be3ed2fe834a8c0afa528e6",
+    Type = "success"
+})
+wait(0.1)
+ConsoleLog:Prompt({
+    Title = "ver-c7e8995823b5e8919243a7a457490110763cb474e8541a8851d56afc9df5a200",
+    Type = "success"
+})
+wait(0.1)
+ConsoleLog:Prompt({
+    Title = "ver-14beb321d64bc5e44e311711b17a034c6e89001538fe2581d1cb21da943439bd",
+    Type = "success"
+})
+wait(0.1)
+ConsoleLog:Prompt({
+    Title = "ver-e54652e99fec0b213ec70ef5c6cad1119a042831ead3c756f5846cfcee39e1d6",
+    Type = "fail"
+})
+wait(0.1)
+ConsoleLog:Prompt({
+    Title = "ver-34974e290ce6e17c229dcfac26eb3c83227daece8e5d9694598f143b80464450",
+    Type = "warning"
+})
+wait(0.1)
+ConsoleLog:Prompt({
+    Title = "ver-a483f69ad6f299e35fb9de84dda97c0822075d54eb5fe9e5814f728938f9f29c",
+    Type = "success"
+})
+wait(0.1)
+ConsoleLog:Prompt({
+    Title = "ver-81b31bcac3f542b257eef394ef59248469f589200c09398a196f1af00b478c42",
+    Type = "fail"
+})
+ConsoleLog:Prompt({
+    Title = "ver-aff4751ae6d06963a9850570d6671abd899f9db92083967973a2d128f12901b0",
+    Type = "fail"
+})
+wait(0.1)
+ConsoleLog:Prompt({
+    Title = "ver-f02ebcdef6e2beb7d15e34f4ebdb084f9b82e8198f830860a61613bfcb82536f",
+    Type = "success"
+})
+wait(0.1)
+ConsoleLog:Prompt({
+    Title = "ver-1f6ead12bffa21b2ceccb450684decdd0ed21b5b7be3ed2fe834a8c0afa528e6",
+    Type = "warning"
+})
+wait(0.1)
+ConsoleLog:Prompt({
+    Title = "ver-c7e8995823b5e8919243a7a457490110763cb474e8541a8851d56afc9df5a200",
+    Type = "warning"
+})
+wait(0.1)
+ConsoleLog:Prompt({
+    Title = "ver-14beb321d64bc5e44e311711b17a034c6e89001538fe2581d1cb21da943439bd",
+    Type = "warning"
+})
+wait(0.1)
+ConsoleLog:Prompt({
+    Title = "ver-e54652e99fec0b213ec70ef5c6cad1119a042831ead3c756f5846cfcee39e1d6",
+    Type = "fail"
+})
+wait(0.1)
+ConsoleLog:Prompt({
+    Title = "ver-34974e290ce6e17c229dcfac26eb3c83227daece8e5d9694598f143b80464450",
+    Type = "warning"
+})
+wait(0.1)
+ConsoleLog:Prompt({
+    Title = "ver-a483f69ad6f299e35fb9de84dda97c0822075d54eb5fe9e5814f728938f9f29c",
+    Type = "fail"
+})
+wait(0.1)
+ConsoleLog:Prompt({
+    Title = "ver-81b31bcac3f542b257eef394ef59248469f589200c09398a196f1af00b478c42",
+    Type = "success"
+})
+wait(0.1)
+ConsoleLog:Prompt({
+    Title = "ver-aff4751ae6d06963a9850570d6671abd899f9db92083967973a2d128f12901b0",
+    Type = "warning"
+})
+wait(0.1)
+ConsoleLog:Prompt({
+    Title = "Check Completed",
+    Type = "Success"
+})
+wait(0.1)
+local Fluent = loadstring(game:HttpGet("https://github.com/dawid-scripts/Fluent/releases/latest/download/main.lua"))()
+Fluent:Notify({
+    Title = "Console Check Done",
+    Content = "Script will load shortly",
+    SubContent = "Xy-Hub Vice City [Beta]",
+    Duration = 5
+})
+wait(5)
+local NiggerXy = game:GetService("CoreGui"):FindFirstChild("STX_Console")
+NiggerXy:Destroy()
+local Fluent = loadstring(game:HttpGet("https://github.com/dawid-scripts/Fluent/releases/latest/download/main.lua"))()
+local SaveManager = loadstring(game:HttpGet("https://raw.githubusercontent.com/dawid-scripts/Fluent/master/Addons/SaveManager.lua"))()
+local InterfaceManager = loadstring(game:HttpGet("https://raw.githubusercontent.com/New2th/Omega-X/main/interface.lua"))()
+local Window = Fluent:CreateWindow({
+    Title = "VyHub - Vice City " .. Fluent.Version,
+    SubTitle = "by xylex",
+    TabWidth = 160,
+    Size = UDim2.fromOffset(580, 460),
+    Acrylic = false,
+    Theme = "Dark",
+    MinimizeKey = Enum.KeyCode.LeftControl
+})
+Fluent:Notify({
+        Title = "VyHub",
+        Content = "Script Loaded Successfully",
+        SubContent = "",
+        Duration = 5
+})
+local Options = Fluent.Options
+local Tabs = {
+    Autofarm = Window:AddTab({ Title = "Autofarm", Icon = "dollar-sign" }),
+    Teleport = Window:AddTab({ Title = "Teleport", Icon = "user" }),
+    Misc = Window:AddTab({ Title = "Misc", Icon = "disc" }),
+    Settings = Window:AddTab({ Title = "Settings", Icon = "settings" })
+}
+Window:SelectTab(1)
+
+--//farm shityyyyy
+Tabs.Autofarm:AddButton({
+    Title = "pm",
+    Callback = function()
+        game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(832.832581, 27.6508236, 971.107666, 0, 0, -1, 0, 1, 0, 1, 0, 0)
+    end
+})
+Tabs.Autofarm:AddButton({
+    Title = "laptop",
+    Callback = function()
+        game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(13.6495228, 166.055664, 337.680176, 0.999346793, 1.27788375e-08, -0.0361377187, -1.19249286e-08, 1, 2.38448372e-08, 0.0361377187, -2.33983233e-08, 0.999346793)
+    end
+})
+
+--//Teleports
+
+Tabs.Teleport:AddParagraph({
+    Title = "Teleport Tools",
+    Content = "These tools can help you with obtaining more money or for other purposes."
+})
+local CandyEaters = Tabs.Teleport:AddSection("Consumers")
+Tabs.Teleport:AddButton({
+    Title = "Darren White",
+    Callback = function()
+        game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(392.982086, 27.2792397, -93.8837204, -1, 0, 0, 0, 1, 0, 0, 0, -1)
+    end
+})
+Tabs.Teleport:AddButton({
+    Title = "Mohammed Brown",
+    Callback = function()
+        game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(626.276794, 19.3904839, 40.7224007, -1, 0, 0, 0, 1, 0, 0, 0, -1)
+    end
+})
+Tabs.Teleport:AddButton({
+    Title = "Mr James",
+    Callback = function()
+        game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(490.61908, 27.4601402, -1516.65491, 0, 0, 1, 0, 1, -0, -1, 0, 0)
+    end
+})
+Tabs.Teleport:AddButton({
+    Title = "Mr Reekz",
+    Callback = function()
+        game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(149.766953, 28.9680004, -108.046356, 0, 0, 1, 0, 1, -0, -1, 0, 0)
+    end
+})
+Tabs.Teleport:AddButton({
+    Title = "Samantha Green",
+    Callback = function()
+        game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(276.94043, 27.1574459, 1037.9259, 1, 0, 0, 0, 1, 0, 0, 0, 1)
+    end
+})
+Tabs.Teleport:AddButton({
+    Title = "Will Smith",
+    Callback = function()
+        game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(-347.82843, 27.606554, -135.992081, 1, 0, 0, 0, 1, 0, 0, 0, 1)
+    end
+})
+local Atm = Tabs.Teleport:AddSection("Atm")
+Tabs.Teleport:AddButton({
+    Title = "Barber Shop Atm",
+    Description = "Takes you to ATM1 in DEX.",
+    Callback = function()
+        game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(597.309082, 27.0027676, -384.681427, -1, 0, 0, 0, 1, 0, 0, 0, -1)
+    end
+})
+Tabs.Teleport:AddButton({
+    Title = "Gas Station Atm",
+    Description = "Takes you to ATM2 in DEX.",
+    Callback = function()
+        game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(-1.93346369, 28.8612881, -51.0580597, -0.99928844, 0, -0.037716005, 0, 1, 0, 0.037716005, 0, -0.99928844)
+    end
+})
+Tabs.Teleport:AddButton({
+    Title = "Miami Threads Atm",
+    Description = "Takes you to ATM3 in DEX.",
+    Callback = function()
+        game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(498.597168, 26.8981991, 1093.27734, -0.037725687, 0, 0.999288201, 0, 1, 0, -0.999288201, 0, -0.037725687)
+    end
+})
+Tabs.Teleport:AddButton({
+    Title = "Apartment Depot Atm",
+    Description = "Takes you to ATM4 in DEX.",
+    Callback = function()
+        game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(498.597168, 26.8981991, 1093.27734, -0.037725687, 0, 0.999288201, 0, 1, 0, -0.999288201, 0, -0.037725687)
+    end
+})
+Tabs.Teleport:AddButton({
+    Title = "Random1 Atm",
+    Description = "Takes you to ATM5 in DEX.",
+    Callback = function()
+        game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(-325.576996, 26.2532387, -527.030457, 0.0377256274, -0, -0.999288201, 0, 1, -0, 0.999288201, 0, 0.0377256274)
+    end
+})
+Tabs.Teleport:AddButton({
+    Title = "East Side Atm",
+    Description = "Takes you to ATM5 in DEX.",
+    Callback = function()
+        game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(-325.576996, 26.2532387, -527.030457, 0.0377256274, -0, -0.999288201, 0, 1, -0, 0.999288201, 0, 0.0377256274)
+    end
+})
+Tabs.Teleport:AddButton({
+    Title = "McDonalds Atm",
+    Description = "Takes you to ATM6 in DEX.",
+    Callback = function()
+        game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(112.711853, 28.2182426, -94.5588989, 0.0377256274, -0, -0.999288201, 0, 1, -0, 0.999288201, 0, 0.0377256274)
+    end
+})
+Tabs.Teleport:AddButton({
+    Title = "Organic Foods Atm",
+    Description = "Takes you to ATM7 in DEX.",
+    Callback = function()
+        game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(395.328491, 26.9885826, -338.763, -0.99928844, 0, -0.037716005, 0, 1, 0, 0.037716005, 0, -0.99928844)
+    end
+})
+Tabs.Teleport:AddButton({
+    Title = "Car Dealer Atm",
+    Description = "Takes you to ATM8 in DEX.",
+    Callback = function()
+        game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(138.868301, 27.4316311, -1219.76611, 0.999288499, 0, 0.037716005, 0, 1, 0, -0.037716005, 0, 0.999288499)
+    end
+})
+Tabs.Teleport:AddButton({
+    Title = "Chicken Shop Atm",
+    Description = "Takes you to ATM9 in DEX.",
+    Callback = function()
+        game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(796.59845, 26.9602814, -1346.40576, 0.999288499, 0, 0.037716005, 0, 1, 0, -0.037716005, 0, 0.999288499)
+    end
+})
+Tabs.Teleport:AddButton({
+    Title = "Gun Shop Atm",
+    Description = "Takes you to ATM10 in DEX.",
+    Callback = function()
+        game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(495.380371, 26.8981991, 895.032776, -0.037725687, 0, 0.999288201, 0, 1, 0, -0.999288201, 0, -0.037725687)
+    end
+})
+Tabs.Teleport:AddButton({
+    Title = "Pharmacy / Punchmade Atm",
+    Description = "Takes you to ATM11 in DEX.",
+    Callback = function()
+        game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(834.117859, 26.9304066, 948.320007, 0.0377256274, -0, -0.999288201, 0, 1, -0, 0.999288201, 0, 0.0377256274)
+    end
+})
+local Npc = Tabs.Teleport:AddSection("Npc")
+Tabs.Teleport:AddButton({
+    Title = "Seed Man",
+    Description = "Provides you with NUM planting materials.",
+    Callback = function()
+        game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(957.977051, 27.4993477, -556.929077, 1, 0, 0, 0, 1, 0, 0, 0, 1)
+    end
+})
+Tabs.Teleport:AddButton({
+    Title = "Lester",
+    Description = "Lets you sell your rewards from the Nightclub Robbery.",
+    Callback = function()
+        game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(994.45459, 27.1933479, 949.185242, -1, 0, 0, 0, 1, 0, 0, 0, -1)
+    end
+})
+Tabs.Teleport:AddButton({
+    Title = "Punchmade",
+    Description = "Sells blank cards for atm hacking.",
+    Callback = function()
+        game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(832.832581, 27.6508236, 971.107666, 0, 0, -1, 0, 1, 0, 1, 0, 0)
+    end
+})
+Tabs.Teleport:AddButton({
+    Title = "Nightclub Robbery Room",
+    Description = "Teleports you inside the cash room of Nightclub.",
+    Callback = function()
+        game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(610.141296, 27.5683441, 1228.33044, -0.186388418, 8.76667627e-08, 0.982476115, 6.63972841e-08, 1, -7.66339951e-08, -0.982476115, 5.09500602e-08, -0.186388418)
+    end
+})
+local BusStop = Tabs.Teleport:AddSection("Bus Stops")
+Tabs.Teleport:AddButton({
+    Title = "Apartment 1",
+    Description = "Takes you to the bus stop at APT1.",
+    Callback = function()
+        game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(801.894592, 27.4342842, 625.035034, 0.277962923, 2.98580952e-11, 0.960591793, 3.784138e-10, 1, -1.40583239e-10, -0.960591793, 4.0257811e-10, 0.277962923)
+    end
+})
+Tabs.Teleport:AddButton({
+    Title = "Car Dealer",
+    Description = "Takes you to the bus stop at Car Dealer.",
+    Callback = function()
+        game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(98.2003403, 27.6464195, -1355.77344, 1, 1.153039e-09, 4.5885578e-08, -1.15304255e-09, 1, 7.73947235e-08, -4.5885578e-08, -7.73947235e-08, 1)
+    end
+})
+Tabs.Teleport:AddButton({
+    Title = "Chicken Shop",
+    Description = "Takes you to the bus stop at Chicken Shop.",
+    Callback = function()
+        game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(746.973328, 27.7499447, -1354.87402, 1, -1.66882241e-08, -1.62446249e-14, 1.66882241e-08, 1, 1.79970332e-08, 1.59442873e-14, -1.79970332e-08, 1)
+    end
+})
+Tabs.Teleport:AddButton({
+    Title = "Gun Shop",
+    Description = "Takes you to the bus stop at Gun Shop.",
+    Callback = function()
+        game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(491.589447, 27.4623451, 930.720764, 1, 2.1168308e-08, -2.07884532e-13, -2.1168308e-08, 1, -2.7875128e-08, 2.07294454e-13, 2.7875128e-08, 1)
+    end
+})
+Tabs.Teleport:AddButton({
+    Title = "Luxury Apartment",
+    Description = "Teleports you to the bus stop at Luxury Apartment.",
+    Callback = function()
+        game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(814.772827, 24.2714882, -1369.31506, -0.998613954, 0, 0.052632276, 0, 1.00000012, 0, -0.0526322685, 0, -0.998614073)
+    end
+})
+
+--//Misc
+
+Tabs.Misc:AddParagraph({
+    Title = "Extra tools",
+    Content = "These functions have been added into VyHub for miscellaneous purposes."
+})
+Tabs.Misc:AddButton({
+    Title = "Teleport to loading zone",
+    Description = "Takes you to the player loading zone, PVP ALLOWED",
+    Callback = function()
+        game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(4481.31299, 27.5443058, -89.0319366, -0.00330310361, 5.38029887e-08, 0.999994516, -1.17788112e-07, 1, -5.41923484e-08, -0.999994516, -1.17966472e-07, -0.00330310361)
+    end
+})
+Tabs.Misc:AddButton({
+    Title = "LOAD ESP",
+    Description = "Loads up Unnamed Esp from ice3w0lf22#2234",
+    Callback = function()
+        pcall(function() loadstring(game:HttpGet('https://raw.githubusercontent.com/ic3w0lf22/Unnamed-ESP/master/UnnamedESP.lua'))() end)
+    end
+})
+Tabs.Misc:AddButton({
+    Title = "LOAD FREECAM",
+    Description = "Loads up freecam from kiwi#9139",
+    Callback = function()
+        pcall(function() loadstring(game:HttpGet('https://pastebin.com/raw/gPDEvqRA'))() end)
+    end
+})
+
+SaveManager:SetLibrary(Fluent)
+InterfaceManager:SetLibrary(Fluent)
+InterfaceManager:BuildInterfaceSection(Tabs.Settings)
+SaveManager:BuildConfigSection(Tabs.Settings)
+Tabs.Settings:AddButton({
+    Title = "Unload",
+    Description = "Unloads VyHub 1.1.0",
+    Callback = function()
+    Fluent:Destroy()
+    end
+})
